40cb1d1b76e66adec15c7d4c56015c4aaf3c1e2c
madhushreen2020 <madhushreen2020@srishakthi.ac.in>
2020-02-18 14:20:35 +0530

    Bug fix

---
+++ MVC Project/Controllers/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class Booking : Controller
+    {
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BookingController(int firstNumber,int secondNumber)
+        {
+            firstNumber = 4;
+            secondNumber = 4;
+            int total = firstNumber + secondNumber;
+            return View(total);
+        }
+    }
+}
